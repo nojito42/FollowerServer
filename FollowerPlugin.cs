ea60f1387a4a4d844f3a6a7445aca50750692c78
nojito42 <nojito42@gmail.com>
2025-06-18 16:17:10 +0200

    add

--- FollowerPlugin.cs
+++ FollowerPlugin.cs
@@ -122,7 +122,7 @@ public class FollowerPlugin : BaseSettingsPlugin<FollowerPluginSettings>
         
 
         if (!GameController.IngameState.InGame || MenuWindow.IsOpened || !GameController.Window.IsForeground()) return;
-
+        LogMessage("Checking for flagged panels...", 0.5f);
         if (!GameController.CloseWindowIfOpen())
         {
             LogMessage("No flagged panels found, continuing...", 0.5f);
