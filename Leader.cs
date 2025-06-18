ea42da9f73ba5509e0a7dd6d45e0b74b5ae9e06e
nojito42 <nojito42@gmail.com>
2025-06-18 17:58:04 +0200

    add

--- Leader.cs
+++ Leader.cs
@@ -15,6 +15,6 @@ public class Leader
     public Entity LastTargetedPortalOrTransition { get; set; }
     public string LeaderCurrentArea => Element?.ChildCount ==4 ? Element.Children[2].Text : Core.Current.GameController.Area.CurrentArea.Name;
 
-    public Entity Entity => Core.Current.GameController.Entities
+    public Entity Entity => Core.Current.GameController.EntityListWrapper.ValidEntitiesByType[EntityType.Player]
         .FirstOrDefault(entity => entity != null && entity.IsValid && entity.Type == EntityType.Player && entity.GetComponent<Player>().PlayerName == Element[0].Text);
 }
