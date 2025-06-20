58e5de3fbdabb5cf99af9e973b333e32fb4bd211
nojito42 <nojito42@gmail.com>
2025-06-20 03:44:18 +0200

    add

--- FollowerPlugin.cs
+++ FollowerPlugin.cs
@@ -301,7 +301,7 @@ public class FollowerPlugin : BaseSettingsPlugin<FollowerPluginSettings>
                     .ToList();
                 foreach (var crySkill in crySkills)
                 {
-                    if(GameController.Player.Buffs.Any(b=> b.DisplayName == crySkill.Name))
+                    if(GameController.Player.Buffs.Any(b=> b.IsInvisible == false && (b.SourceSkill == crySkill)))
                     {
                         LogError($"Cry Skill {crySkill.Name} is already active, skipping.");
                         continue;
